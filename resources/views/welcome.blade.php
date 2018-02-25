db02311f9c27204d99c460dbed54eb68c71519db
Peter <sppaulsson@gmail.com>
2018-02-25 18:48:46 +0100

    First page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Nyhems samfällighet
+                    Nyhem
                 </div>
 
                 
